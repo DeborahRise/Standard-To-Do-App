218cca1a59c60b02b0c08368162cc9868d96e36c
DeborahRise <debie.rise.bookings@gmail.com>
2024-06-19 16:25:54 +0100

    Implement task deletion and addition functionality in todo_list.js. Ensure tasks can be added with a timestamp and deleted using the delete button. Improved overall task handling logic.

--- script/todo_list.js
+++ script/todo_list.js
@@ -4,12 +4,19 @@ let schedule =[];
 const displayTodo = () => {
     let todoHtml = "";
     for (let i = 0; i<taskList.length; i++) {
-        todoHtml += ` <p> ${taskList[i] || 'empty task'} ${schedule[i] || '--'} 
-        <button onclick="deleteTask(${i})">Delete</button></p>`
+        const taskId = `task-${i}`
+        todoHtml += `<div id="${taskId}" class="tasklist" style="margin-bottom: 10px;">
+            <div>${taskList[i] || 'empty task'}</div>
+            <div> ${schedule[i] || '--'} </div>
+            <button class="delete-button" onclick="deleteTask(${i})">Delete</button>
+            <select name="TaskStatus" id="TaskStatus" onchange="handleChange(${i}, this)">
+                    <option value="undone">Undone</option>
+                    <option value="done">Done</option>
+                </select>
+        </div>`;
     }
     document.querySelector('.js-tasklist').innerHTML = todoHtml;
 }
-
 
 const addTask = () => {
     let taskName = document.querySelector('.js-input-task').value;
@@ -25,5 +32,19 @@ const deleteTask = (index) => {
     schedule.splice(index, 1)
     displayTodo();
 };
+
+const handleChange = (index, selectElement) => {
+    const selectedOption = selectElement.value;
+    const taskId = `task-${index}`;
+    const taskElement = document.getElementById(taskId);
+
+    if (selectedOption === 'done') {
+        taskElement.classList.remove("tasklist")
+        taskElement.classList.add("tasklist2")
+    } else if (selectedOption === 'undone')  {
+        taskElement.classList.remove("tasklist2")
+        taskElement.classList.add("tasklist")
+    }
+}
 
 console.log('vaerify', taskList)
